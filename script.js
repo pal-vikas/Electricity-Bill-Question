efb61fb63190b632d735d3bf76a6af913386e938
Vikas Pal <vikaspal@Vikass-MacBook-Air.local>
2025-06-29 19:17:55 +0530

    E-Bill

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-// let unit = Number(prompt("Enter your units consumed:"));
-// let amount = 0;
+let unit = Number(prompt("Enter your units consumed:"));
+let amount = 0;
 
-// if (unit > 300) {
-//     amount += (unit - 300) * 11;
-//     unit = 300;
-// }
+if (unit > 300) {
+    amount += (unit - 300) * 11;
+    unit = 300;
+}
 
-// if (unit > 150 && unit <= 300) {
-//     amount += (unit - 150) * 7;
-//     unit = 150;
-// }
+if (unit > 150 && unit <= 300) {
+    amount += (unit - 150) * 7;
+    unit = 150;
+}
 
-// if (unit > 60 && unit <= 150) {
-//     amount += (unit - 60) * 4.5;
-//     unit = 60;
-// }
+if (unit > 60 && unit <= 150) {
+    amount += (unit - 60) * 4.5;
+    unit = 60;
+}
 
-// if (unit > 0 && unit <= 60) {
-//     amount += unit * 2;
-// }
+if (unit > 0 && unit <= 60) {
+    amount += unit * 2;
+}
 
-// console.log("Total Bill: ₹" + amount);
+console.log("Total Bill: ₹" + amount);
